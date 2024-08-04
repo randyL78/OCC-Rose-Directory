50d7ab6702f0ecb6285e3e1cf6b6ea87f71af183
Randy Layne <randydavidl78@gmail.com>
2024-08-04 15:07:05 -0400

    Add code coverage for client tests

--- rose-client/vite.config.ts
+++ rose-client/vite.config.ts
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      provider: "istanbul"
+    },
     environment: 'happy-dom',
     globals: true,
     setupFiles: './tests/setup.ts',
