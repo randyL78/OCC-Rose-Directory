10fe68350f9a9222bd050ca9a63904646cb04bb0
Randy Layne <randydavidl78@gmail.com>
2024-08-04 14:24:12 -0400

    Add features to CI/CD pipeline. Refactor pipeline * Add linting to client pipeline * Add testing to client pipeline * Extract common code for client commit stage * Extract common code for client staging and prod deployments

--- rose-client/.eslintrc.cjs
+++ rose-client/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -10,6 +10,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
