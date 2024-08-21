415a2a56ca50b958b782532c50a9caefe77d4a12
Randy Layne <randydavidl78@gmail.com>
2024-08-21 10:58:01 -0400

    Add scroll restoration component so that navigating from bottom of Rose Index does not scroll to bottom of a Rose Detail

--- rose-client/src/components/Layout.tsx
+++ rose-client/src/components/Layout.tsx
@@ -1,17 +1,20 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, ScrollRestoration} from "react-router-dom";
 import {Box} from "@mui/material";
 
 function Layout() {
   return (
-    <Box
-      sx={{
-        position: 'relative',
-        pb: 4,
-        minHeight: '100vh',
-      }}
-    >
-      <Outlet />
-    </Box>
+    <>
+      <ScrollRestoration />
+      <Box
+        sx={{
+          position: 'relative',
+          pb: 4,
+          minHeight: '100vh',
+        }}
+      >
+        <Outlet />
+      </Box>
+    </>
   )
 }
 
